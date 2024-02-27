4b9bf32e2ef8641fb405e5a778b8c011e0a8eb58
23knightst813 <23knightst813@collyers.ac.uk>
2024-02-27 15:59:45 +0000

    added send message functionality

--- ToDo.md
+++ ToDo.md
@@ -4,12 +4,22 @@ Login System
 
 ### Column Name
 
-- [ ] - Message Functionlaity
-- [ ] - Server Side User Sessions
-- [ ] - Test The Code For Bugs
-- [ ] - Add Password Hashing
-- [ ] - Add Input Valadation
+# Today
+
+    - [ ] - Message Functionlaity
+
+        - [ ] - Inbox system
+        - [✓] - Sending System
+
+    - [ ] - Server Side User Sessions
+    - [ ] - Add Password Hashing
+    - [ ] - Add Input Valadation
+
+- [ ] - Add unique usernames in account creation
 - [ ] - Design And Impliment A Pretty GUI
 - [ ] - Login With Google
+- [ ] - Enable Two-Factor Authentication
+- [ ] - Test The Code For Bugs
+- [ ] - Refactoring ( Use multiple pyton files)
 
 ### Completed Column ✓
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import psycopg2
 import psycopg2.extras
-from flask import Flask, redirect, render_template, request, session, url_for, make_response
+from flask import Flask, redirect, render_template, request, session, url_for, make_response, flash
+import datetime
 
 app = Flask(__name__)
 app.secret_key = "three"
 
 
-@app.route("/" , )
+@app.route("/"  )
 def home():
 
     email = "Guest"
+    print("keurfe")
 
     if "email" not in session:
         return redirect(url_for("login"))
@@ -134,6 +136,42 @@ def deleteaccount():
         return render_template("deleteaccount.html")
 
 
+@app.route('/Message', methods=["GET", "POST"])
+def send_message_page():
+
+
+
+        print('SenMessgaeFlask')
+
+        if request.method == "POST":
+
+            print('post')
+
+            TARGET_EMAIL = request.form["email"]
+            TARGET_MESSAGE = request.form["Message"]
+            print(TARGET_EMAIL)
+            print(TARGET_MESSAGE)
+
+            x = send_message(TARGET_EMAIL, TARGET_MESSAGE)
+
+            '''
+            if x = True
+                print('Message sent successfully!', 'success')
+            if x = False
+                print('Failed to send message!', 'error')
+            '''
+
+            return render_template('Message.html', status = x)
+
+
+        if request.method == "GET":
+            print('post')
+
+            return render_template('Message.html')
+
+
+
+
 try:
     conn = psycopg2.connect(
         dbname="database",
@@ -150,45 +188,7 @@ except:
     print("Not connected")
 
 
-def check_table_exists():
-    print("Checking if table exists")
-    cur = conn.cursor()
-    cur.execute(
-        """
-        SELECT EXISTS (
-            SELECT 1
-            FROM information_schema.tables
-            WHERE table_name = 'users'
-        )
-        """
-    )
-    result = cur.fetchone()[0]
-    print(result)
 
-    if result is False:
-        print("Table does not exist")
-        cur.execute(
-            """
-            CREATE TABLE users (
-                ID SERIAL PRIMARY KEY,
-                Email VARCHAR(255) UNIQUE NOT NULL,
-                Password VARCHAR(255) NOT NULL
-            )
-            """
-        )
-        cur.execute(
-            """
-            INSERT INTO users (Email, Password)
-            VALUES 
-            ('khumes2g@vk.com', 'rO2!uGjl0/'),
-            ('tnorcliff2h@discovery.com', 'yF5\\H9tr>'),
-            ('gpennycock2i@xinhuanet.com', 'lI2$Tu8vv#'),
-            ('fsliman2k@state.tx.us', 'nE0$*X{<9HzR')
-            """
-        )
-    else:
-        print("Table exists")
-    print("Data added to the users table")
 
 
 def check_login(EMAIL, PASSWORD):
@@ -270,11 +270,166 @@ def delete_account(EMAIL, PASSWORD):
         return True
 
 
-def give_session():
-    print("Giving session")
+def check_users_table_exists():
+    print("Checking if table exists")
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_name = 'users'
+        )
+        """
+    )
+    result = cur.fetchone()[0]
+    print(result)
+
+    if result is False:
+        print("Table does not exist")
+        cur.execute(
+            """
+            CREATE TABLE users (
+                ID SERIAL PRIMARY KEY,
+                Email VARCHAR(255) UNIQUE NOT NULL,
+                Password VARCHAR(255) NOT NULL
+            )
+            """
+        )
+        cur.execute(
+            """
+            INSERT INTO users (Email, Password)
+            VALUES 
+            ('khumes2g@vk.com', 'rO2!uGjl0/'),
+            ('tnorcliff2h@discovery.com', 'yF5\\H9tr>'),
+            ('gpennycock2i@xinhuanet.com', 'lI2$Tu8vv#'),
+            ('fsliman2k@state.tx.us', 'nE0$*X{<9HzR')
+            """
+        )
+    else:
+        print("Table exists")
+    print("Data added to the users table")
 
 
-check_table_exists()
+def check_messages_table_exsits():
+    print('checking if messages table is present')
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables
+            WHERE table_name = 'messages'
+        )
+        """
+    )
+
+    result = cur.fetchone()[0]
+    print(result)
+
+    if result is False:
+        print("Table does not exist")
+        cur.execute(
+            """
+                CREATE SEQUENCE messages_message_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 18 CACHE 1;
+
+                CREATE TABLE "public"."messages" (
+                    "message_id" integer DEFAULT nextval('messages_message_id_seq') NOT NULL,
+                    "receiver_id" integer NOT NULL,
+                    "sent_id" integer NOT NULL,
+                    "message" text NOT NULL,
+                    "time" timestamp,
+                    CONSTRAINT "messages_pkey" PRIMARY KEY ("message_id")
+                ) WITH (oids = false);
+
+                COMMENT ON COLUMN "public"."messages"."message_id" IS 'The mail id ';
+
+                COMMENT ON COLUMN "public"."messages"."receiver_id" IS 'The id of the person who got the message';
+
+                COMMENT ON COLUMN "public"."messages"."sent_id" IS 'Hold the guy who sent it';
+
+                COMMENT ON COLUMN "public"."messages"."message" IS 'Hold the messgae';
+
+                COMMENT ON COLUMN "public"."messages"."time" IS 'the time';
+
+
+                ALTER TABLE ONLY "public"."messages" ADD CONSTRAINT "messages_receiver_id_fkey" FOREIGN KEY (receiver_id) REFERENCES users(id) NOT DEFERRABLE;
+                ALTER TABLE ONLY "public"."messages" ADD CONSTRAINT "messages_sent_id_fkey" FOREIGN KEY (sent_id) REFERENCES users(id) NOT DEFERRABLE;
+            
+            """
+        )
+
+    else:
+        print("Table exists")
+
+
+
+def send_message(TARGET_EMAIL, TARGET_MESSAGE):
+    print('Send Message')
+
+    # Get the sender id
+
+    #get the email
+    sender_email = session["email"]
+
+    #sql the email to get the id
+
+    cur = conn.cursor()
+    cur.execute('''
+
+        SELECT id
+        FROM users
+        WHERE Email = %s
+
+''', (sender_email,))
+    sender_id = cur.fetchone()
+
+    #find the targets id ( receiver )
+
+    receiver_email = TARGET_EMAIL
+
+    cur = conn.cursor()
+    cur.execute('''
+
+            SELECT id
+            FROM users
+            WHERE Email = %s
+
+''', (receiver_email,))
+
+    receiver_id = cur.fetchone()
+
+
+    # Get the message they wanna send
+
+    Message = TARGET_MESSAGE
+
+    Time_Date = datetime.datetime.now()
+
+
+    #Add to data base
+
+    if receiver_id or sender_id or Message == None or '':
+
+        print('Error')
+        return 'False'
+
+    else:
+
+        cur.execute('''
+            INSERT INTO "messages" (receiver_id, sent_id, message , time) VALUES (%s, %s, %s , %s)
+        ''', (receiver_id, sender_id, Message , Time_Date.strftime("%c")))
+
+        print('Sent')
+
+        return 'True'
+
+
+
+
+
+check_users_table_exists()
+check_messages_table_exsits()
 
 if __name__ == "__main__":
     app.run(debug=True)
